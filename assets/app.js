073f3ff0a6ec9c4800ab5bc3a621120cc5a0cf19
mgmcintyre777 <mgmcintyre777@gmail.com>
2017-10-27 16:44:24 -0400

    pretty much complete

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,26 @@
 "use strict";
-const buttonLabels = ["dog", "cat", "batman", "javascript", "happy", "meme"];
+
+const buttonLabels = ["dog", "cat", "batman", "javascript", "happy", "turtle"];
 const maxImages = 10;
-const $div_buttonsArea = $("#buttons_area");
-const $div_gifsArea = $("#gifs_area");
 
-$div_buttonsArea.append(generateButtons());
+const $div_buttonsArea = $("#buttons-area");
+const $div_gifsArea = $("#gifs-area");
+const $btn_inputButton = $("#input-button");
+const $frm_inputTextbox = $("#input-textbox");
 
-function generateButtons(){
+$div_buttonsArea.append(generateButtons(buttonLabels));
+$btn_inputButton.on("click", onClick_addButton);
 
-	return buttonLabels.map(function(label){
+function generateButtons(labelsArray){
+
+	return labelsArray.map(function(label){
 
 		return $("<button type='button'>")
 			.addClass("btn btn-primary btn-sm mt-1 mr-1")
 			.attr("label", label)
 			.html(label)
-			.css( 'cursor', 'pointer' )
+			.css('cursor', 'pointer')
 			.on("click", onClick_labelButton);
-
 	});
 }
 
@@ -25,16 +29,19 @@ function generateImageDivs(imageData){
 	return imageData.map(function(data){
 
 		var $img_gif = $("<img>")
+			.addClass("figure-img img-fluid rounded")
 			.attr("src", data.images.fixed_height_still.url)
 			.attr("still_url", data.images.fixed_height_still.url)
 			.attr("animate_url", data.images.fixed_height.url)
 			.css('cursor', 'pointer')
 			.on("click", onClick_gifImage);
 
-		var $p_rating = $("<p>")
+		var $p_rating = $("<figcaption>")
+			.addClass("figure-caption")
 			.html("Rating: " + data.rating.toUpperCase());
 
-		var $div_gif = $("<div>")
+		var $div_gif = $("<figure>")
+			.addClass("figure float-left mr-1")
 			.append($img_gif)
 			.append($p_rating);
 
@@ -56,8 +63,11 @@ function getGiffy(searchTerm){
 }
 
 function onClick_addButton(){
-
-
+	var input = $frm_inputTextbox.val().trim();
+	if(input){
+		$div_buttonsArea.append(generateButtons([input]));
+		getGiffy(input);
+	}
 }
 
 function onClick_gifImage(){	
